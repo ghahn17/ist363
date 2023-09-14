76245b8c24c120b7ec097464340b9c1a60427b61
Gillian Hahn <gwhahn@syr.edu>
2023-09-14 18:17:29 -0400

    Added objects to the array. made it dynamic so we can now make a loop with more objects

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -34,7 +34,7 @@ closeBtn.addEventListener("click",function(){
 const coffeeName = "Mocha"; 
 const coffeeHeadline = document.createElement("h2"); 
 coffeeHeadline.textContent = coffeeName; 
-document.body.appendChild(coffeeHeadline); 
+// document.body.appendChild(coffeeHeadline); 
 
 // objects 
 const coffee = {
@@ -47,11 +47,76 @@ const coffee = {
 
 const coffeeObjHeadline = document.createElement("h2");
 coffeeObjHeadline.textContent = coffee.name; 
-document.body.appendChild(coffeeObjHeadline);
+//document.body.appendChild(coffeeObjHeadline); 
 
 // arrays 
-const coffees = ["Mocha", "Latte", "Espresso"];
+// const coffees = ["Mocha", "Latte", "Espresso"];
+// curly braces = objects , brackets = arrays. 
+const coffees =[ 
+    { 
+        name: "Mocha",
+        price: 3.99,
+        description: "This is a delicious cup of coffee.",
+        image: { 
+            fileName: "coffee.jpg",
+            width: 630,
+            height: 630,
+            altText: "A cup of coffee."
+        }
+    },
+    {
+        name: "Latte",
+        price: 3.99,
+        description: "This is a delicious cup of coffee.",
+        image: { 
+            fileName: "coffee.jpg",
+            width: 630,
+            height: 630,
+            altText: "A cup of coffee."
+        }
+    },
+    {
+        name: "Espresso",
+        price: 3.99,
+        description: "This is a delicious cup of coffee.",
+        image: { 
+            fileName: "coffee.jpg",
+            width: 630,
+            height: 630,
+            altText: "A cup of coffee."
+        }
+    }, 
+] 
+// this is an array of objects 
 //console.log(coffees[1]);
 coffees.forEach(function(coffee) {
-    console.log(coffee);
-});
+    //1. deconstruct the coffee object 
+    const {name,price,description, image} = coffee;
+
+//2. create html elements 
+    const coffeeArticle = document.createElement("article")
+
+    const coffeeImage = document.createElement("img")
+    coffeeImage.src=  `images/${image.fileName}`; 
+    coffeeImage.width = image.width;
+    coffeeImage.width = image.height;
+    coffeeImage.alt = image.altText; 
+
+    // const coffeeTitle = coffee;
+    const coffeeName = document.createElement("h2");
+    coffeeName.textContent = name;
+   
+    const coffeePrice = document.createElement("h3");
+    coffeePrice.textContent= price;
+
+    const coffeeDescription = document.createElement("p");
+    coffeeDescription.textContent= description;
+//3. append the elements to the parent article 
+    coffeeArticle.appendChild(coffeeImage);
+    coffeeArticle.appendChild(coffeeName);
+    coffeeArticle.appendChild(coffeePrice);
+    coffeeArticle.appendChild(coffeeDescription); 
+//4.append the article to the body 
+    document.body.appendChild(coffeeArticle)
+}); 
+// end of coffees forEach method 
